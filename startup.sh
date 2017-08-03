7f350d82b734994552ec0565d6023d51a0ac0421
Keith Hoopes <xitikit.org@gmail.com>
2017-08-03 08:23:57 -0600

    Updated bash version to have the same steps as the windows bat version

--- startup.sh
+++ startup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
-exec ./mvnw clean package -U
-export CATALINA_HOME="./apache-tomcat-8.0.45"
-rm -rf ${CATALINA_HOME}/webapps/
-mkdir -p ${CATALINA_HOME}/webapps/
-rm -rf ${CATALINA_HOME}/logs/
-mkdir -p ${CATALINA_HOME}/logs/
+export CATALINA_HOME=$PWD/apache-tomcat-8.0.45
+export JAVA_OPTS="-Dgoodbye.config.location=%cd%/config/goodbye -Dhello.config.location=%cd%/config/hello"
+
+exec mvnw clean package -U
+rm -rf %CATALINA_HOME%/webapps/hello
+rm -f %CATALINA_HOME%/webapps/hello.war
+rm -rf %CATALINA_HOME%/webapps/goodbye
+rm -f %CATALINA_HOME%/webapps/goodbye.war
+rm -rf %CATALINA_HOME%/logs
+mkdir -p %CATALINA_HOME%/logs
 cp ./hello/target/hello.war %CATALINA_HOME%/webapps/hello.war
 cp ./goodbye/target/goodbye.war %CATALINA_HOME%/webapps/goodbye.war
-exec ${CATALINA_HOME}/bin/startup.sh
+exec %CATALINA_HOME%/bin/startup.sh
