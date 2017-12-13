322081ba43d923df553912eabe4e4c1fde3317f1
Keith Hoopes <keith.hoopes@brmutual.com>
2017-12-13 09:53:03 -0700

    Just some formatting I guess.

--- hello/src/main/java/com/example/remotelogging/HelloServletInitializer.java
+++ hello/src/main/java/com/example/remotelogging/HelloServletInitializer.java
@@ -6,42 +6,39 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
 @Slf4j
-public class HelloServletInitializer
-    extends SpringBootServletInitializer
-{
+public class HelloServletInitializer extends SpringBootServletInitializer{
 
-  @Value("${messageForUser}")
-  private String message;
+    @Value("${messageForUser}")
+    private String message;
 
-  @Value("${whatDoesTheFoxSay:'No body knows.'}")
-  private String whatDoesTheFoxSay;
+    @Value("${whatDoesTheFoxSay:'No body knows.'}")
+    private String whatDoesTheFoxSay;
 
-  public static void main(String[] args) {
+    public static void main(String[] args){
 
-    SpringApplication.run(HelloServletInitializer.class, args);
-  }
+        SpringApplication.run(HelloServletInitializer.class, args);
+    }
 
-  @Scheduled(fixedRate = 2000)
-  public void sayHelloTo() {
+    @Scheduled(fixedRate = 2000)
+    public void sayHelloTo(){
 
-    log.info("Hello! " + message);
-  }
+        log.info("Hello! " + message);
+    }
 
-  @Override
-  public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder application){
 
-    log.info(
-        "\n*********************\n" +
-            "What does the fox say?\n" +
-            whatDoesTheFoxSay +
-            "\n*********************\n");
-    return application.sources(HelloServletInitializer.class);
-  }
+        log.info(
+            "\n*********************\n" +
+                "What does the fox say?\n" +
+                whatDoesTheFoxSay +
+                "\n*********************\n");
+        return application.sources(HelloServletInitializer.class);
+    }
 }
